e60eef3aad5f0baa5bfacbfa31495a1966778b9f
ungHeung <t01047377477@gmail.com>
2023-10-16 03:29:06 +0900

    feat : 필터링을 위한 selector 추가

--- src/atom/todoListAtom.ts
+++ src/atom/todoListAtom.ts
@@ -1,4 +1,4 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 import { todoListProps } from "../pages/todoList/TodoItemCreator";
 import { recoilPersist } from "recoil-persist";
 
@@ -14,4 +14,26 @@ export const todoListState = atom({
   key: "todoListState",
   default: [] as todoListProps[],
   effects_UNSTABLE: [persistAtom],
+});
+
+export const todoListFilterState = atom({
+  key: "todoListFilterState",
+  default: "Show All",
+});
+
+export const filteredTodoListState = selector({
+  key: "filteredTodoListState",
+  get: ({ get }) => {
+    const filter = get(todoListFilterState);
+    const list = get(todoListState);
+
+    switch (filter) {
+      case "Show Completed":
+        return list.filter((item: todoListProps) => item.isComplete);
+      case "Show Uncompleted":
+        return list.filter((item: todoListProps) => !item.isComplete);
+      default:
+        return list;
+    }
+  },
 });
