4b1ff957915f2774f24ca3c32941169f06568ccd
ungHeung <t01047377477@gmail.com>
2023-10-16 02:53:17 +0900

    feat : 투두 아이템 생성

--- src/pages/todoList/TodoItemCreator.tsx
+++ src/pages/todoList/TodoItemCreator.tsx
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+import { useRecoilState, useSetRecoilState } from "recoil";
+import { todoIndexState, todoListState } from "../../atom/todoListAtom";
+
+export interface todoListProps {
+  id: number;
+  text: string;
+  isComplete: boolean;
+}
 
 const TodoItemCreator = () => {
-  return <div>TodoItemCreator</div>;
+  const [inputValue, setInputValue] = useState(""); // react state
+  const [todoIndex, setTodoIndex] = useRecoilState(todoIndexState);
+  const setTodoList = useSetRecoilState<todoListProps[]>(todoListState); // recoil state setter
+
+  const addItem = () => {
+    setTodoList((oldTodoList): todoListProps[] => [
+      ...oldTodoList,
+      {
+        id: todoIndex,
+        text: inputValue,
+        isComplete: false,
+      } as todoListProps,
+    ]);
+
+    setTodoIndex(todoIndex + 1);
+    setInputValue("");
+  };
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+      }}
+    >
+      <input type="text" value={inputValue} onChange={onChange} />
+      <button onClick={addItem}>Add</button>
+    </form>
+  );
 };
 
 export default TodoItemCreator;
