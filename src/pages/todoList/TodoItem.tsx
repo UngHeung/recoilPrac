89efb27fe29ab232facb0fa4c67c2799ba6933ea
ungHeung <t01047377477@gmail.com>
2023-10-16 02:52:49 +0900

    feat : 투두 목록, 업데이트, 삭제 기능

--- src/pages/todoList/TodoItem.tsx
+++ src/pages/todoList/TodoItem.tsx
@@ -1,8 +1,39 @@
 import React from "react";
+import { todoListState } from "../../atom/todoListAtom";
+import { useRecoilState } from "recoil";
+import { todoListProps } from "./TodoItemCreator";
 
 const TodoItem = ({ ...props }) => {
-  const todoItem = props.todoItem;
-  return <div>TodoItem</div>;
+  const item = props.item as todoListProps;
+  const [todoList, setTodoList] = useRecoilState(todoListState);
+
+  const editItemText = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newList = todoList.map((todo: todoListProps) =>
+      todo.id === item.id ? { ...todo, text: e.target.value } : todo
+    );
+
+    setTodoList(newList);
+  };
+
+  const toggleItemCompletion = () => {
+    const newList = todoList.map((todo: todoListProps) =>
+      todo.id === item.id ? { ...todo, isComplete: !item.isComplete } : todo
+    );
+    setTodoList(newList);
+  };
+
+  const deleteItem = () => {
+    const newList = todoList.filter((todo: todoListProps) => todo.id !== item.id);
+    setTodoList(newList);
+  };
+
+  return (
+    <div>
+      <input type="text" value={item.text} onChange={editItemText} />
+      <input type="checkbox" checked={item.isComplete} onChange={toggleItemCompletion} />
+      <button onClick={deleteItem}>X</button>
+    </div>
+  );
 };
 
 export default TodoItem;
