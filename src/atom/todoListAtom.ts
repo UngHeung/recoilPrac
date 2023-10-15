e99be2988c2f1009637342dec80135d649d9df25
ungHeung <t01047377477@gmail.com>
2023-10-16 02:53:49 +0900

    feat : 임시 인덱스 상태 atom 추가

--- src/atom/todoListAtom.ts
+++ src/atom/todoListAtom.ts
@@ -1,6 +1,17 @@
 import { atom } from "recoil";
+import { todoListProps } from "../pages/todoList/TodoItemCreator";
+import { recoilPersist } from "recoil-persist";
+
+const { persistAtom } = recoilPersist();
+
+export const todoIndexState = atom({
+  key: "todoIndex",
+  default: 0,
+  effects_UNSTABLE: [persistAtom],
+});
 
 export const todoListState = atom({
   key: "todoListState",
-  default: [],
+  default: [] as todoListProps[],
+  effects_UNSTABLE: [persistAtom],
 });
