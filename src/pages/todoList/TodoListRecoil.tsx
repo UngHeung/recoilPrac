3c6f811c6cbe435cc0c0d31a4d65dd7a534819b9
ungHeung <t01047377477@gmail.com>
2023-10-16 03:29:36 +0900

    feat : 필터링 적용

--- src/pages/todoList/TodoListRecoil.tsx
+++ src/pages/todoList/TodoListRecoil.tsx
@@ -1,17 +1,22 @@
 import React from "react";
 import TodoItemCreator, { todoListProps } from "./TodoItemCreator";
 import TodoItem from "./TodoItem";
-import { todoListState } from "../../atom/todoListAtom";
+import { filteredTodoListState, todoListState } from "../../atom/todoListAtom";
 import { useRecoilValue } from "recoil";
+import TodoListFilters from "./TodoListFilters";
+import TodoListStates from "./TodoListStates";
 
 const TodoListRecoil = () => {
-  const todoList = useRecoilValue(todoListState);
+  // const todoList = useRecoilValue(todoListState);
+  const todoList = useRecoilValue(filteredTodoListState);
 
   return (
     <>
+      <TodoListStates />
+      <TodoListFilters />
       <TodoItemCreator />
 
-      {todoList.map((todoItem: todoListProps, idx: number) => (
+      {todoList?.map((todoItem: todoListProps, idx: number) => (
         <TodoItem key={`todoItem_${idx}`} item={todoItem} />
       ))}
     </>
---
+++ src/pages/todoList/TodoListStates.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TodoListStates = () => {
+  return <div>TodoListState</div>;
+};
+
+export default TodoListStates;
