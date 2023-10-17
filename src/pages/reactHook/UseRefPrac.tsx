bcf2c9aa99a7540efe4941cc39445ad76bc5b748
ungHeung <t01047377477@gmail.com>
2023-10-17 19:02:50 +0900

    feat : useRef를 사용하여 리렌더링없이 여러개의 인풋 관리

--- src/pages/reactHook/UseRefPrac.tsx
+++ src/pages/reactHook/UseRefPrac.tsx
@@ -1,7 +1,47 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 const UseRefPrac = () => {
-  return <div>UseRefPrac</div>;
+  const firstInput = useRef("");
+  const secondInput = useRef("");
+  const thirdInput = useRef("");
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>, type: React.MutableRefObject<string>) => {
+    type.current = e.target.value;
+  };
+
+  useEffect(() => {
+    console.log(1);
+  });
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    console.log(firstInput, secondInput, thirdInput);
+  };
+
+  const reset = () => {
+    firstInput.current = "";
+    secondInput.current = "";
+    thirdInput.current = "";
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="first_input">first input</label>
+      <input type="text" name="firstInput" id="first_input" onChange={(e) => handleChange(e, firstInput)} />
+      <br />
+      <label htmlFor="second_input">second input</label>
+      <input type="text" name="secondInput" id="second_input" onChange={(e) => handleChange(e, secondInput)} />
+      <br />
+      <label htmlFor="third_input">third input</label>
+      <input type="text" name="thirdInput" id="third_input" onChange={(e) => handleChange(e, thirdInput)} />
+      <br />
+      <button>console</button>
+      <button type="button" onClick={reset}>
+        reset
+      </button>
+    </form>
+  );
 };
 
 export default UseRefPrac;
